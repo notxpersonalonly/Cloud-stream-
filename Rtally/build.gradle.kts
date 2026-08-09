fe82b0e1daebd1f6172cb6ca2ea64ea27799f54a
notxpersonalonly <notxpersonalonly@gmail.com>
2026-08-09 21:54:59 +0530

    Move Rtally to root

---
+++ Rtally/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 46
+
+cloudstream {
+    description = "Rtally — Movies, Series, Anime and Asian Drama"
+    authors = listOf("Redowan", "NivinCNC")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "AnimeMovie", "AsianDrama")
+    iconUrl = "https://www.google.com/s2/favicons?domain=rtally.net&sz=%size%"
+    language = "en"
+}

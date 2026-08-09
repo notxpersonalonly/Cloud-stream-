37e80de12bb5aef5f4c05eafd89fbd5a7d821428
notxpersonalonly <notxpersonalonly@gmail.com>
2026-08-09 21:53:37 +0530

    Move CricifyProvider to root

---
+++ CricifyProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 65
+
+cloudstream {
+    description = "Cricify — Live Cricket and Sports Streaming"
+    authors = listOf("NivinCNC")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=cricify.live&sz=%size%"
+    language = "en"
+}

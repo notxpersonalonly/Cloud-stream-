680b5d1f1bca23ced8732acb32e253b1a3121aea
notxpersonalonly <notxpersonalonly@gmail.com>
2026-08-09 21:54:01 +0530

    Move CastleTvProvider to root

---
+++ CastleTvProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 38
+
+cloudstream {
+    description = "CastleTV — Movies and TV Series"
+    authors = listOf("NivinCNC")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=castletv.net&sz=%size%"
+    language = "en"
+}

91e392069437678bda61e721ed180c88c8b113f5
notxpersonalonly <notxpersonalonly@gmail.com>
2026-08-09 21:55:20 +0530

    Move StreamFlixProvider to root

---
+++ StreamFlixProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 32
+
+cloudstream {
+    description = "StreamFlix — Movies, TV Series and Anime"
+    authors = listOf("NivinCNC")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=streamflix.com.co&sz=%size%"
+    language = "en"
+}

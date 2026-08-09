23c6fc52ce755c0d505b7c44acc708110f027fdf
notxpersonalonly <notxpersonalonly@gmail.com>
2026-08-09 21:54:47 +0530

    Move PikashowProvider to root

---
+++ PikashowProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 30
+
+cloudstream {
+    description = "Pikashow — Movies, Web Series and Live TV"
+    authors = listOf("NivinCNC")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=pikashow.com&sz=%size%"
+    language = "hi"
+}
